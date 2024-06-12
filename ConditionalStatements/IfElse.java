fe88298fc57a733c1611a1248ef7c7888ee38293
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-06-12 23:22:03 +0530

    Add files via upload

---
+++ ConditionalStatements/IfElse.java
@@ -0,0 +1,16 @@
+package ConditionalStatements;
+
+public class IfElse {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+        if (a > 0){
+            System.out.println("a is greater");
+        }
+        else{
+            System.out.println("a is smaller");
+        }
+        
+    }
+}
