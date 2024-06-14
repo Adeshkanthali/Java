9bbc74b08009d49a3417c49642608000c06ee69e
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-06-14 23:01:40 +0530

    Add files via upload

---
+++ String/StringCompare.java
@@ -0,0 +1,19 @@
+package Github.String;
+
+public class StringCompare {
+
+    public static void main(String[] args) {
+
+        String a = "adesh";                      //String Literal
+        String b = new String("adesh");   //New Keyword
+
+        if(a == b){
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+
+    }
+
+}
