63991de28191fa0bd6d1258b8098290dec30f683
Adesh Kanthali <92244882+Adeshkanthali@users.noreply.github.com>
2024-06-26 23:51:27 +0530

    Add files via upload

---
+++ CollectionFramework/ArrayList/ArrayListDemo.java
@@ -0,0 +1,19 @@
+package Github.CollectionFramework.ArrayList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListDemo {
+
+    public static void main(String[] args) {
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        System.out.println(list);
+    }
+
+}
---
+++ CollectionFramework/LinkedList/LinkedListDemo.java
@@ -0,0 +1,17 @@
+package Github.CollectionFramework.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+    public static void main(String[] args) {
+
+        LinkedList<Integer> list1 = new LinkedList<>();
+        list1.add(10);
+        list1.add(20);
+        list1.add(20);
+        list1.add(40);
+
+        System.out.println(list1);
+
+    }
+}
